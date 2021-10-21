f4536638339dcfffb9b0ff7d7196c8291371ee0d
jhyj0521 <jhyj3636@gmail.com>
2021-10-21 18:58:23 +0900

    feat: mudule folder

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,9 @@
 const getRandomColor = () =>
-  '#' + ('000000' + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+  '#' +
+  ('000000' + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
 
 const getRandomNum = () => Math.floor(Math.random() * 10);
 
-export { getRandomColor, getRandomNum };
+const getFontSizeRemovedPerPixel = fontSize => fontSize.replace(/px/, '');
+
+export { getRandomColor, getRandomNum, getFontSizeRemovedPerPixel };
